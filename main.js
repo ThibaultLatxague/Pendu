839fd03a7ee8ebeb84b97103a081f4e7ffcc56b6
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-09-19 10:39:33 +0200

    Ajout debut de fonction pour l'input utilisateur

---
+++ main.js
@@ -0,0 +1,10 @@
+function getInputUtilisateur() {
+    let input = document.getElementById('inputTexte');
+    let motCorrect = false;
+
+    input.addEventListener('submit', (elt) => {
+        let reponse = elt.target.value;
+    });
+}
+
+getInputUtilisateur();
