78b65c06c7ec1c052dee827a8822d286bdb34592
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-09-20 11:51:52 +0200

    Ajout nombre erreurs et gestion 1 ou plusieurs lettres

--- main.js
+++ main.js
@@ -2,37 +2,40 @@ function getInputUtilisateur() {
     let form = document.getElementById('formElement'); // get le form
     let input = document.getElementById('inputTexte'); // get l'input
     let mot = generationMot();
-    let motTrouve = false;
 
     form.addEventListener('submit', (event) => {
         event.preventDefault(); // empeche le formulaire de recharger la page
         let reponse = input.value.trim().toLowerCase(); // réponse en minuscule
         input.value=""; // on clear l'input pour pouvoir entrer du texte directement
-        motTrouve = gererInput(reponse, mot); // appel de la fonction qui gere le résultat de l'input
-
-        if (motTrouve) {
-            input.disabled = true;
-        }
+        gererInput(reponse, mot); // appel de la fonction qui gere le résultat de l'input
     });
 }
 
 getInputUtilisateur();
 
 function gererInput(reponse, mot) {
+    let input = document.getElementById('inputTexte'); // get l'input
     let liste = document.getElementById('ulLettres');
-    let trouve = false;
+    let erreurs = 0;
 
-    if (mot.includes(reponse)) {
+    if (mot.includes(reponse) && reponse.length === 1) {
         let li = document.createElement('li');
         li.textContent = reponse;
         liste.appendChild(li);
         console.log('comprend la lettre !');
-    } else if (reponse === mot) {
+    } else if (reponse == mot) {
         console.log(mot + " a été trouvé !");
-        trouve = true;
+        input.disabled = true;
+        input.placeholder = 'Mot trouvé !';
+    } else {
+        erreurs ++;
+        console.log("Nombre d'erreurs : " + erreurs);
+        if (erreurs == 10) {
+            input.disabled = true;
+        }
     }
 
-    return trouve;
+    console.log("Taille du mot : " + reponse.length);
 }
 
 function generationMot() {
