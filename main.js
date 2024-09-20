de47fba6f898c8d1720753b602549812da0d97b5
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-09-20 14:56:20 +0200

    Ajout d'une nouvelle liste (lettres tentees) en complement de lettres trouvees

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let boutonRestart = document.getElementById('restart'); //Get le bouton restart
 let form = document.getElementById('formElement'); //Get le forumlaire
 let input = document.getElementById('inputTexte'); //Get l'input utilisateur
-let liste = document.getElementById('ulLettres'); //Get la liste des lettre essayées
+let listeTentees = document.getElementById('ulLettresTentees'); //Get la liste des lettre essayées
+let listeTrouvees = document.getElementById('ulLettresTrouvees'); //Get la liste des lettre trouvees
 let motAffiche = document.getElementById('wordToFind'); //Get l'elt qui affiche le mot généré
 
 boutonRestart.addEventListener('click', () =>{
@@ -35,8 +36,12 @@ function handleRestart() {
     input.classList.remove('success');
 
     //Suppression de tous les enfants tant qu'il y en a un
-    while (liste.firstChild) {
-        liste.removeChild(liste.firstChild);
+    while (listeTentees.firstChild) {
+        listeTentees.removeChild(listeTentees.firstChild);
+    };
+
+    while (listeTrouvees.firstChild) {
+        listeTrouvees.removeChild(listeTrouvees.firstChild);
     };
 
     getInputUtilisateur(); //On redémarre le processus : génération de mot etc
@@ -44,7 +49,6 @@ function handleRestart() {
 
 function gererInput(reponse) {
     mot = motAffiche.textContent;
-    let changement = true;
     let erreurs = 0;
     console.log("Fonction gererInput mot : " + mot);
 
@@ -52,7 +56,7 @@ function gererInput(reponse) {
     if (mot.includes(reponse) && reponse.length === 1) {
         let li = document.createElement('li');
         li.textContent = reponse;
-        liste.appendChild(li);
+        listeTrouvees.appendChild(li);
     } 
 
     // Vérifier si le mot COMPLET est trouvé
@@ -68,6 +72,9 @@ function gererInput(reponse) {
         if (erreurs == 10) {
             input.disabled = true;
         }
+        let li = document.createElement('li');
+        li.textContent = reponse;
+        listeTentees.appendChild(li);
     }
 }
 
