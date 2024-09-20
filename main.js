84ca872983f5930f1fc4425945573da4ec0df332
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-09-20 11:28:41 +0200

    Desac input si mot trouvé

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 function getInputUtilisateur() {
     let form = document.getElementById('formElement'); // get le form
     let input = document.getElementById('inputTexte'); // get l'input
+    let mot = generationMot();
+    let motTrouve = false;
 
     form.addEventListener('submit', (event) => {
         event.preventDefault(); // empeche le formulaire de recharger la page
         let reponse = input.value.trim().toLowerCase(); // réponse en minuscule
         input.value=""; // on clear l'input pour pouvoir entrer du texte directement
-        gererInput(reponse); // appel de la fonction qui gere le résultat de l'input
+        motTrouve = gererInput(reponse, mot); // appel de la fonction qui gere le résultat de l'input
+
+        if (motTrouve) {
+            input.disabled = true;
+        }
     });
 }
 
 getInputUtilisateur();
 
-function gererInput(param) {
-    console.log(param);
+function gererInput(reponse, mot) {
+    let trouve = false;
+
+    if (reponse === mot) {
+        console.log(mot + " a été trouvé !");
+        trouve = true;
+    }
+
+    return trouve;
 }
 
 function generationMot() {
-    console.log('cc');
+    let motATrouver = 'boule';
+    return motATrouver;
 }
