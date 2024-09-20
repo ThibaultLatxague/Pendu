d479c59b1100502a58d52d382e679020155a1cbe
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-09-20 22:42:56 +0200

    Ajout de commentaires et espacement de code

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let boutonRestart = document.getElementById('restart'); //Get le bouton restart
-let form = document.getElementById('formElement'); //Get le forumlaire
 let input = document.getElementById('inputTexte'); //Get l'input utilisateur
 let listeTentees = document.getElementById('ulLettresTentees'); //Get la liste des lettre essayées
 let motAffiche = document.getElementById('wordToFind'); //Get l'elt qui affiche le mot généré
 let nombreLettresAffichees = document.getElementById('nbrLettres');
 let motCache = document.getElementById('wordToFindHidden');
 let mot = '';
+let erreurs = 0;
 
 boutonRestart.addEventListener('click', () =>{
-    handleRestart();
+    handleRestart(); //A chaque clique, on déclenche la fonction
 });
 
 getInputUtilisateur();
@@ -16,13 +16,15 @@ getInputUtilisateur();
 let formListenerAdded = false; // Ajout d'une variable pour suivre l'ajout de l'écouteur
 
 async function getInputUtilisateur() {
-    mot = await generationMot();
-    afficheExtremitesMot(mot);
-    console.log(mot);
+    let form = document.getElementById('formElement'); //Get le forumlaire
+
+    mot = await generationMot(); //On attend l'exec complète de la fonction
+    afficheExtremitesMot(mot); //On affiche le mot sur le site (avec espaces et _)
+    console.log(mot); //Sympa pour le débug dans la console
 
     // On vérifie si l'écouteur a déjà été ajouté pour éviter la duplication
     if (!formListenerAdded) {
-        form.addEventListener('submit', (event) => {
+        form.addEventListener('submit', (event) => { //Déclenchement quand appuie bouton form
             event.preventDefault(); // Empêche le rechargement de la page
             let reponse = input.value.trim().toLowerCase(); // Réponse en minuscule
             input.value = ""; // On clear l'input
@@ -33,9 +35,9 @@ async function getInputUtilisateur() {
 }
 
 function handleRestart() {
-    input.disabled = false;
-    input.placeholder = 'Entrez un mot ou caractère...';
-    input.classList.remove('success');
+    input.disabled = false; //On réactive le bouton in case
+    input.placeholder = 'Entrez un mot ou caractère...'; //On remet le placeholder de base ------ Aucun changement si sélectionné
+    input.classList.remove('success'); //On repasse au style de base CSS
 
     //Suppression de tous les enfants tant qu'il y en a un
     while (listeTentees.firstChild) {
@@ -46,9 +48,9 @@ function handleRestart() {
 }
 
 function gererInput(reponse) {
-    ajouterEspaces();
-    let erreurs = 0;
-    let motSansAccents = mot.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    //ajouterEspaces();
+    let erreurs = 0; //On compte le nombre d'erreurs pour notre image
+    let motSansAccents = mot.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); //NON FONCTIONNEL gère les accents
 
     // Vérifier une lettre unique
     if (motSansAccents.includes(reponse) && reponse.length === 1) {
@@ -65,9 +67,11 @@ function gererInput(reponse) {
     else {
         erreurs ++;
         if (erreurs == 10) {
+            //Si le max d'erreurs a été atteint, on désactive la saisie
             input.disabled = true;
         }
 
+        //On créé l'elt liste qu'on affichera ensuite
         let li = document.createElement('li');
         li.textContent = reponse;
         listeTentees.appendChild(li);
@@ -77,15 +81,15 @@ function gererInput(reponse) {
 }
 
 function motEstTrouve() {
-    input.disabled = true;
-        input.placeholder = 'Mot trouvé !';
-        input.classList.add('success');
-        motAffiche.textContent = mot;
+    input.disabled = true; //On désactive le bouton
+    input.placeholder = 'Mot trouvé !'; //On change le placeholder pour un truc zouli
+    input.classList.add('success'); //On change le style dans le CSS
+    motAffiche.textContent = mot;
 }
 
 function metAJourMot(lettre) {
     let motHTML = motAffiche.textContent.split(''); // Convertir la chaîne en tableau pour modification
-    let compteur_ = 0;
+    let compteur_ = 0; //Le compteur de _
 
     for (let i = 0; i < mot.length; i++) {
         const element = mot[i];
@@ -98,6 +102,7 @@ function metAJourMot(lettre) {
         }
     }
 
+    //S'ill n'y a plus de _ alors c'est gagné
     if (compteur_ == 0) {
         motEstTrouve();
     }
@@ -112,6 +117,7 @@ function afficheExtremitesMot(mot) {
     for (let i = 0; i < mot.length; i++) {
         const element = mot[i];
         
+        //On affiche seulement la lettre de début et de fin
         if (i == 0 || i == mot.length - 1) {
             motSecret += element;
         } else {
@@ -119,6 +125,7 @@ function afficheExtremitesMot(mot) {
         }
     }
 
+    //On stock tout ça pour le récup autre part
     motAffiche.textContent = motSecret;
     motCache.textContent = motSecret;
     ajouterEspaces();
@@ -128,12 +135,12 @@ function ajouterEspaces() {
     let motSansEspaces = motAffiche.textContent;
     let motAvecEspaces = '';
     
+    //On créé un nouveau mot qui a 1 lettre du mot original pour 1 espace
     for (let i = 0; i < motSansEspaces.length; i++) {
         const element = motSansEspaces[i];
         motAvecEspaces += element + ' ';
     }
 
-    console.log(motAvecEspaces);
     motCache.textContent = motAvecEspaces;
 }
 
