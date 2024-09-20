ab8223c447d19c44435e8e333b58a9fe554c2333
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-09-20 14:35:02 +0200

    Au relancement partie : 2 mots sont compris dans le mot unique a trouver

--- main.js
+++ main.js
@@ -9,14 +9,15 @@ boutonRestart.addEventListener('click', () =>{
 
 getInputUtilisateur();
 
-
 async function getInputUtilisateur() {
     let mot = await generationMot();
-    console.log(mot);
+
+    //On attend le clic sur le sumbit
     form.addEventListener('submit', (event) => {
         event.preventDefault(); // empeche le formulaire de recharger la page
         let reponse = input.value.trim().toLowerCase(); // réponse en minuscule
         input.value=""; // on clear l'input pour pouvoir entrer du texte directement
+        console.log('Affichage mot adns getInputUtilisateur : ' + mot);
         gererInput(reponse, mot); // appel de la fonction qui gere le résultat de l'input
     });
 }
@@ -30,35 +31,42 @@ function handleRestart() {
     while (liste.firstChild) {
         liste.removeChild(liste.firstChild);
     };
+
+    getInputUtilisateur(); //On redémarre le processus : génération de mot etc
 }
 
 function gererInput(reponse, mot) {
+    let changement = true;
     let erreurs = 0;
+    console.log("Fonction gererInput mot : " + mot);
 
     // Vérifier une lettre unique
     if (mot.includes(reponse) && reponse.length === 1) {
         let li = document.createElement('li');
         li.textContent = reponse;
         liste.appendChild(li);
-        console.log('comprend la lettre !');
+        //console.log('comprend la lettre !');
     } 
+
     // Vérifier si le mot COMPLET est trouvé
     else if (reponse == mot) {
-        console.log(mot + " a été trouvé !");
+        //console.log(mot + " a été trouvé !");
         input.disabled = true;
         input.placeholder = 'Mot trouvé !';
         input.classList.add('success');
     } 
+
     // Si rien n'est trouvé
     else {
         erreurs ++;
-        console.log("Nombre d'erreurs : " + erreurs);
+        //console.log("Nombre d'erreurs : " + erreurs);
         if (erreurs == 10) {
             input.disabled = true;
         }
     }
 
-    console.log("Taille du mot : " + reponse.length);
+    //console.log("Taille du mot : " + reponse.length);
+    mot = '';
 }
 
 async function generationMot() {
