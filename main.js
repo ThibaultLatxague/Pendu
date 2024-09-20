d2712719a2f32f0bf8232e561e823465555629b1
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-09-20 14:50:15 +0200

    Suppression de console.log et ajout de commentaires

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let boutonRestart = document.getElementById('restart');
-let form = document.getElementById('formElement'); // get le form
-let input = document.getElementById('inputTexte'); // get l'input
-let liste = document.getElementById('ulLettres');
-let motAffiche = document.getElementById('wordToFind');
+let boutonRestart = document.getElementById('restart'); //Get le bouton restart
+let form = document.getElementById('formElement'); //Get le forumlaire
+let input = document.getElementById('inputTexte'); //Get l'input utilisateur
+let liste = document.getElementById('ulLettres'); //Get la liste des lettre essayées
+let motAffiche = document.getElementById('wordToFind'); //Get l'elt qui affiche le mot généré
 
 boutonRestart.addEventListener('click', () =>{
     handleRestart();
@@ -53,12 +53,10 @@ function gererInput(reponse) {
         let li = document.createElement('li');
         li.textContent = reponse;
         liste.appendChild(li);
-        //console.log('comprend la lettre !');
     } 
 
     // Vérifier si le mot COMPLET est trouvé
     else if (reponse == mot) {
-        //console.log(mot + " a été trouvé !");
         input.disabled = true;
         input.placeholder = 'Mot trouvé !';
         input.classList.add('success');
@@ -67,14 +65,10 @@ function gererInput(reponse) {
     // Si rien n'est trouvé
     else {
         erreurs ++;
-        //console.log("Nombre d'erreurs : " + erreurs);
         if (erreurs == 10) {
             input.disabled = true;
         }
     }
-
-    //console.log("Taille du mot : " + reponse.length);
-    mot = '';
 }
 
 async function generationMot() {
@@ -83,7 +77,6 @@ async function generationMot() {
         const data = await response.json();
         const mots = data.mots;
         const motATrouver = mots[Math.floor(Math.random() * mots.length)];
-        console.log(motATrouver); // Afficher le mot sélectionné
         return motATrouver; // Retourner le mot
     } catch (error) {
         console.error('Erreur de chargement du fichier JSON:', error);
